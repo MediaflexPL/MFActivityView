4b15a33721b04987d507ec33ba37eebf62c45ddd
Jakub Darowski <jdarowski@MacBook-Pro-Jakub.local>
2016-02-27 16:49:42 +0100

    some old stuff

---
+++ Example/Pods/Target Support Files/MFActivityView/MFActivityView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MFActivityView : NSObject
+@end
+@implementation PodsDummy_MFActivityView
+@end
---
+++ Example/Pods/Target Support Files/MFActivityView/MFActivityView-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "MFActivityView.h"
+
+FOUNDATION_EXPORT double MFActivityViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char MFActivityViewVersionString[];
+
